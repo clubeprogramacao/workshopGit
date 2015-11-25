fcd3d523fa2987f8f9ccd81e879bcaf325d2acee
Francisco Couceiro <couceiro.f@gmail.com>
2015-11-25 17:17:14 +0000

    Node.js boilerplate

---
+++ server.js
@@ -0,0 +1,10 @@
+//imports
+var express 	= require('express');
+var app 		= express();
+var port 	 	= Number(process.env.PORT || 8080);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+	console.log("listening on " + port);
+});
